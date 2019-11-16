2994b8a2d724885cef6d98b636738061016bb7a4
karapv <andrewkarapenkov@gmail.com>
2019-11-16 13:31:23 +0200

    Header + Slider is done

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+$(document).ready(function () {
+   /* Header submenu */
+   $('.header-submenu').parent().addClass('active-sub');
+   /* Header Multilang */
+   $('.multi-lang-item.active a').click(function () {
+      $(this).parent().next('.multi-lang-item').css({'display':'block'});
+   });
+   $('.multi-lang-item a').click(function () {
+       let lang = $(this).text();
+       $('.multi-lang-item.active a').text(lang);
+       if($(this).parent().hasClass('active')){
+           console.log('changed')
+       }else{
+           $(this).parent().css({'display':'none'});
+       }
+   });
+   /* Video slider */
+    $('.video-slider').slick({
+       arrows: true,
+       draggable: true,
+       infinite: true,
+       swipeToSlide: true,
+       slidesToShow: 3,
+       slidesToScroll: 3,
+       swipe: true,
+       prevArrow: '<button type="button" class="video-slider-left icofont-simple-left"></button>',
+       nextArrow: '<button type="button" class="video-slider-right icofont-simple-right"></button>'
+    });
 });
